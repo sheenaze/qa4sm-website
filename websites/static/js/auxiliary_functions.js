bb07550a2540072e758b1e58fd6a7e92f7f61944
sheenaze <tercjak@awst>
2021-02-01 13:28:27 +0100

    js file with basic funtions created

--- websites/static/js/auxiliary_functions.js
+++ websites/static/js/auxiliary_functions.js
@@ -1,1 +1,22 @@
-console.log("I'm here")
+// showing and hiding main section of a news on click on Read more and Read less
+function show_body(obj){
+  var hidingClass = 'hide_element';
+	var currentObject = obj.currentTarget;
+  var newsMainBody = $(currentObject).parent().siblings('section.news_main');
+  var readLess = $(newsMainBody).children('a.read_less');
+
+  $(newsMainBody).attr('hidden', false);
+  $(currentObject).attr('hidden', true);
+  $(readLess).attr('hidden', false);
+}
+
+function hide_body(obj){
+  var hidingClass = 'hide_element';
+	var currentObject = obj.currentTarget;
+  var newsMainBody = $(currentObject).parent();
+  var readMore = $(newsMainBody).siblings('section.news_intro').children('a.read_more');
+
+  $(newsMainBody).attr('hidden', true);
+  $(currentObject).attr('hidden', true);
+  $(readMore).attr('hidden', false);
+}
