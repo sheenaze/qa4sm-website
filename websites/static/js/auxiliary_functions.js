411626be0fab4619ab4993041e4b9649afbb1dd9
sheenaze <tercjak@awst>
2021-02-02 12:45:59 +0100

    functions show_body and hide_body changed to be more universal

--- websites/static/js/auxiliary_functions.js
+++ websites/static/js/auxiliary_functions.js
@@ -1,22 +1,18 @@
 // showing and hiding main section of a news on click on Read more and Read less
 function show_body(obj){
-  var hidingClass = 'hide_element';
-	var currentObject = obj.currentTarget;
-  var newsMainBody = $(currentObject).parent().siblings('section.news_main');
-  var readLess = $(newsMainBody).children('a.read_less');
+  var currentObject = obj.currentTarget;
+  var newsMainBody = $(currentObject).closest('.container').find('section.news_main');
 
   $(newsMainBody).attr('hidden', false);
   $(currentObject).attr('hidden', true);
-  $(readLess).attr('hidden', false);
+
 }
 
 function hide_body(obj){
-  var hidingClass = 'hide_element';
-	var currentObject = obj.currentTarget;
-  var newsMainBody = $(currentObject).parent();
-  var readMore = $(newsMainBody).siblings('section.news_intro').children('a.read_more');
+  var currentObject = obj.currentTarget;
+  var newsMainBody = $(currentObject).closest('section.news_main');
+  var readMore = $(newsMainBody).closest('.container').find('a.read_more');
 
   $(newsMainBody).attr('hidden', true);
-  $(currentObject).attr('hidden', true);
   $(readMore).attr('hidden', false);
 }
